5344ad247635021c994b3052c56480fbb245ae98
YZ-Bruh <131263805+YZ-Bruh@users.noreply.github.com>
2023-08-14 10:45:00 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_8079.mk
-
-COMMON_LUNCH_CHOICES := \
-    omni_8079-user \
-    omni_8079-userdebug \
-    omni_8079-eng
+    $(LOCAL_DIR)/device_8079.mk
